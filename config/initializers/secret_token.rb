c51cf4e6329140218152df73587c80e1566ed15a
Adam Raouki <adam0964@gmail.com>
2014-02-16 19:32:40 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flite::Application.config.secret_key_base = '7ead1d1a9192b31979d30b212d4249617d515758665ed9a9ffbccce473c46a2d76aecdf82c5c9021f8ac73343fb3ac2d7f6a10a0155ddcbc980052b3d9c1c058'
